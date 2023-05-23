aa6d8b3129eb9dfe20a58a32aa5dac047a7b7672
Jamie Place <jamie.place@tranetechnologies.com>
2023-05-23 23:49:09 +0100

    initial commit

---
+++ lua/jamie/init.lua
@@ -0,0 +1,4 @@
+require("jamie.remap")
+require("jamie.set")
+require("jamie.packer")
+require("jamie.reload")
---
+++ lua/jamie/remap.lua
@@ -0,0 +1,35 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- vim.keymap.set('n', '<leader>sv', vim.cmd('source $MYVIMRC'))
+vim.api.nvim_set_keymap("n", "<leader>rl", "<cmd>lua ReloadConfig()<CR>", { noremap = true, silent = false })
+
+vim.keymap.set('n', '<leader><CR>', function()
+
+    local last_line = vim.fn.line('$')
+    local pos = vim.api.nvim_win_get_cursor(0)
+    
+    require('iron.core').send_line()
+    require('iron.core').send(nil,  '\n')
+    vim.notify("Sent line to repl!", vim.log.levels.INFO)
+    vim.api.nvim_win_set_cursor(0, { math.min(pos[1] + 1, last_line), pos[2] })
+
+end)
+vim.keymap.set('v', '<leader><CR>', function()
+
+    local last_line = vim.fn.line('$')
+    local pos = vim.api.nvim_win_get_cursor(0)
+    
+    require('iron.core').visual_send()
+    vim.notify("Sent selection to repl!", vim.log.levels.INFO)
+    vim.api.nvim_win_set_cursor(0, { math.min(pos[1] + 1, last_line), pos[2] })
+
+    end)
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+vim.keymap.set('t', "<Esc>", "<C-\\><C-n>")
+
