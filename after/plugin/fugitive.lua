2019170fa76e3b3043c6584a3ceaff500a575641
JamiePlace <jamieplace95@gmail.com>
2023-05-24 12:53:38 +0100

    added mechanism to push from shortcut

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,20 @@
 vim.keymap.set("n", "<leader>gs", "<cmd>Git status<cr>")
 vim.keymap.set("n", "<leader>ga", "<cmd>Git add %<cr>")
 vim.keymap.set("n", "<leader>gc", function()
-    vim.cmd('Git commit -m ' .. vim.fn.input("Commit Message > "))
+    local message = vim.fn.input("Commit Message > ")
+    local git_placeholder = "Git commit -m 'PLACEHOLDER'"
+    command = string.gsub(git_placeholder, 'PLACEHOLDER', message)
+    vim.cmd(command)
+end)
+
+vim.keymap.set("n", "<leader>gp", function()
+    local prompt = vim.fn.input("Push? [Y]es/[N]o > ")
+    prompt = string.lower(prompt)
+
+    if prompt == 'y' then
+        echo('Pushing!')
+        vim.cmd('Git push')
+    else
+        echo('Not Pushing!')
+    end
 end)
