dbee885cf86b981426e667a520dcc38558382425
JamiePlace <jamieplace95@gmail.com>
2023-12-21 03:42:14 +0000

    removed neotree

--- lua/jamie/lazy.lua
+++ lua/jamie/lazy.lua
@@ -27,24 +27,6 @@ require("lazy").setup({
     end
 },
 {"nvim-treesitter/nvim-treesitter-context"};
--- neotree
-{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-
-        require("neo-tree").setup({
-            close_if_last_window=false,
-        })
-        vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-    end
-},
 -- lsp
 {'VonHeikemen/lsp-zero.nvim',
 branch = 'v1.x',
--- lua/jamie/remap.lua
+++ lua/jamie/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Neotree)
---vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --paste and copy from clipboard
 vim.keymap.set("n", "<leader>pl", '"*p')
